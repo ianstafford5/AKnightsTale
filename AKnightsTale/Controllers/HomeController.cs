9580f8010a805039ecb5f49c4f850b9c4aafbd33
istafford <istafford@live.ie>
2018-04-13 22:51:19 +0100

    test download2

--- AKnightsTale/Controllers/HomeController.cs
+++ AKnightsTale/Controllers/HomeController.cs
@@ -78,7 +78,7 @@ namespace AKnightsTale.Controllers
 
         public FileResult Download()
         {
-            var FileVirtualPath = "~/App_Data/uploads/" + "ChartImg.png";
+            var FileVirtualPath = "~/App_Data/uploads/" + "c74.jpg";
             return File(FileVirtualPath, "application/force-download", Path.GetFileName(FileVirtualPath));
         }
     }
