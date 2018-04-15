9a190ee25ef846686f93033c4cd607297fa1014a
istafford <istafford@live.ie>
2018-04-15 16:29:13 +0100

    added game

--- AKnightsTale/Controllers/HomeController.cs
+++ AKnightsTale/Controllers/HomeController.cs
@@ -80,7 +80,7 @@ namespace AKnightsTale.Controllers
         {
             downloads++;
             Debug.WriteLine(downloads);
-            var FileVirtualPath = "~/App_Data/uploads/" + "c74.jpg";
+            var FileVirtualPath = "~/App_Data/uploads/" + "AKnightsTale.exe";
             return File(FileVirtualPath, "application/force-download", Path.GetFileName(FileVirtualPath));
         }
 
